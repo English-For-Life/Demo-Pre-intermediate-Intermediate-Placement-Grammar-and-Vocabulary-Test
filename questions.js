97b0bf4ddf038fae3a87535599be234551596760
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-11 00:10:57 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+//Array of objects
+const quiz = [
+    {
+        q:"A lot of ____ came to Ireland in the 1990s.",
+        options:["immigrants", "emigrants", "invaders", "colonies"],
+        answer: 0
+    },
+    {
+        q:"There was a nice meal and a band at the wedding ____ .",
+        options:["ceremony", "reception", "speech", "group"],
+        answer: 1
+    },
+    {
+        q:"I mostly ____ my friends via email. ",
+        options:["see each other", "have in common ", "keep in touch with", "get on well with"],
+        answer: 2
+    },
+    {
+        q:"I’ve received 33 emails  ____ .",
+        options:["on Friday", "yesterday", "two days ago", "this week"],
+        answer: 1
+    },
+    {
+        q:"How often have you been to the doctor  ______ ",
+        options:["last week?", "yesterday?", "in the last twelve months?", "one year ago?"],
+        answer: 2
+    },
+    {
+        q:"I was saving up ____ a new computer.",
+        options:["for buying", "-", "to buying", "to buy"],
+        answer: 3
+    },
+    {
+        q:"You ____ wear a suit to work, but you can if you want.",
+        options:["don't have to", "could", "must", "mustn't"],
+        answer: 0
+    },
+    {
+        q:"I had to ____ a uniform to school when I was younger.",
+        options:["wearing", "wear", "have", "having"],
+        answer: 1
+    },
+    {
+        q:"Cecilia knows someone ____ went to the carnival in Rio de Janeiro.",
+        options:["where", "she", "who", "which"],
+        answer: 2
+    },
+    {
+        q:"Oxfam is a charity ____ tries to find lasting solutions to poverty.",
+        options:["it", "who", "-", "which"],
+        answer: 3
+    }
+    
+
+
+]
